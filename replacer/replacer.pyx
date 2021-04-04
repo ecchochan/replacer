05f4d3f5d25e4870b842b3de6cd7dafb2e46d844
ecchochan <ecchochan@gmail.com>
2021-04-05 00:24:26 +0800

    Update replacer.pyx

--- replacer/replacer.pyx
+++ replacer/replacer.pyx
@@ -655,9 +655,9 @@ class Replacer():
                 elif matched == True:
                     pass
                 elif matched == False:
-                    
+
+                    abort = True
                     if len(REPLs) > 0:
-                        abort = True
                         for j in range(len(REPLs), 0, -1):
                             REPL, REPL_pos = REPLs[j]
                             matched = callback(REPL)
